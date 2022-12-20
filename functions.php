9b90e099d48f1d973405416070369d185d1fc48b
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2022-12-20 14:56:27 +0100

    amélioration fonction ajouts/modifs réservations

--- functions.php
+++ functions.php
@@ -1,108 +1,5 @@
 <?php
 
-function create_modify_delete_profile() {
-
-    include 'connecSQL.php';
-
-    $request_user_info= "SELECT * FROM `utilisateurs`";
-    $query_user_info = $mysqli->query($request_user_info);
-    $result_user_info = $query_user_info->fetch_all();
-
-    $message;
-    $check = 1;
-
-    if(isset($_POST['inscription'])){
-
-        if(empty($_POST['pseudo']) || empty($_POST['mdp']) || trim($_POST['pseudo'] == '' || trim($_POST['mdp'])) == '') {
-            
-            $check = 0;
-            $message = 'Certains champs sont vides';
-        }
-
-        if($_POST['mdp'] !== $_POST['mdp_confirm']) {
-            $check = 0;
-            $message =  'Les mots de passe ne correspondent pas';
-        }
-
-        if ($check == 1) {
-
-            for($x = 0; isset($result_user_info[$x]); $x++ ) {
-
-                    if($result_user_info[$x][1] == $_POST['pseudo']) {
-                        $check = 0 ;
-                        $message = 'Ce pseudo existe déjà';
-                    }
-            } 
-        }
-            
-        if($check == 1) {
-
-            $login = $_POST['pseudo'];
-            $mdp_hash = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
-            $request_create = "INSERT INTO `utilisateurs`(`login`, `password`) VALUES ('$login','$mdp_hash')";
-            $query_create = $mysqli->query($request_create);
-
-// Les lignes ci_dessous me permettent de connecter automatiquement l'utilisateur après avoir créé son compte. Ensuite dans connec.php je vais récupérer son ID.
-            if(session_id() == '') {
-                session_start();
-                $_SESSION['user'] = $_POST['pseudo'];
-            }
-                
-            $message =  'Compte créé avec succès. <br> Vous êtes désormais connecté.';
-        }
-    }
-
-    
-
-    if(isset($_POST['profile_change'])) {  
-
-        if(empty($_POST['pseudo']) || empty($_POST['new_mdp']) || trim($_POST['pseudo'] == '' || trim($_POST['new_mdp'])) == '') {
-            $check = 0;
-            $message = 'Certains champs sont vides';
-        }
-
-        if($_POST['new_mdp'] !== $_POST['new_mdp_confirm']) {
-            $check = 0;
-            $message = 'Les nouveaux mots de passe ne correspondent pas';
-        }
-
-        if($check == 1) {
-
-            if(!password_verify($_POST['mdp'], $result_user_info[0][2])) {
-                $check = 0;
-                $message = 'Ancien mot de passe incorrect';
-            }
-
-            if($check == 1) {   
-
-                for($x = 0; isset($result_user_info[$x]); $x++ ) {
-                        if($result_user_info[$x][1] == $_POST['pseudo'] && $result_user_info[$x][0] !== $_SESSION['userID']) {
-                            $check = 0;
-                            $message = 'Ce pseudo existe déjà';
-                        }
-                }
-            }
-        }
-
-        if($check == 1) {
-                
-        $modified_mdp_hashed = password_hash($_POST['new_mdp'], PASSWORD_DEFAULT);
-
-        $update_user_profile = "UPDATE utilisateurs 
-        SET login = '$_POST[pseudo]', password = '$modified_mdp_hashed' 
-        WHERE id= '$_SESSION[userID]'";
-        $query_update_user_profile = $mysqli->query($update_user_profile);
-
-        $message = "informations modifiées.";
-        }
-    }
-
-    return $message;
-    
-    }
-
-
-
     function horaires($debut, $fin, $compare) {
 
             for($x = $debut ; $x <= $fin; $x++) {
@@ -147,92 +44,142 @@ function add_modify_event() {
         $message = "Veuillez remplir tous les champs";
     }
     
-    //Vérification si l'utilisateur tente de réserver un samedi ou dimanche
+    
     if($check == 1) {
-        
-        $date_debut = new DateTime($_POST['date_debut_event']);
-        $date_fin = new DateTime($_POST['date_fin_event']);
+    
+        $debut_event = new DateTime("$_POST[date_debut_event] midnight + $_POST[start_time] hours");      
+        $fin_event = new DateTime("$_POST[date_fin_event] midnight + $_POST[end_time] hours");
 
-        if($date_debut->format('D') == 'Sat' || $date_debut->format('D') == 'Sun' || $date_fin->format('D') == 'Sat' || $date_fin->format('D') == 'Sun') {
+        if($debut_event->format('D') == 'Sat' || $debut_event->format('D') == 'Sun' || $fin_event->format('D') == 'Sat' || $fin_event->format('D') == 'Sun') {
             $check = 0;
             $message = 'Nous sommes fermés les week-ends';
         }
     }
-    
-    if($check == 1) {
-    
-        $date_debut = $_POST['date_debut_event'] . ' ' . $_POST['start_time'];      
-        $date_fin = $_POST['date_fin_event'] . ' ' . $_POST['end_time'];
-    }
         
-    if($date_debut >= $date_fin) {       
+    if($check == 1 && $debut_event->format('Y-m-d H') >= $fin_event->format('Y-m-d H')) {       
         $check = 0;
         $message = 'Erreur dans les créneaux horaires';
     }
             
     if($check == 1) {
-            
-        $request_date_events = "SELECT id, debut, fin FROM reservations WHERE debut BETWEEN '$date_debut' and '$date_fin' OR fin BETWEEN '$date_debut' and '$date_fin'";
+
+        $debut_event_formated = $debut_event->format('Y-m-d H');
+        $fin_event_formated = $fin_event->format('Y-m-d H');
+
+        $request_date_events = "SELECT id, debut, fin FROM reservations WHERE debut BETWEEN '$debut_event_formated' and '$fin_event_formated' OR fin BETWEEN '$debut_event_formated' and '$fin_event_formated'";
         $query_date_events = $mysqli->query($request_date_events);
         $result_date_events = $query_date_events->fetch_all(); 
+      
+        for($x = 0; isset($result_date_events[$x]); $x++){
     
-//Je récupère les dates des évènement déjà programmés aux dates de début et de fin que l'utilisateur a sélectionnées. J'utilise ensuite DateTime pour vérifier 
-//si les créneaux sélectionnées sont déjà pris, grâce à la boucle for du dessous.
-    
-            for($x = 0; isset($result_date_events[$x]); $x++){
-    
-                $date_debut_SQL = new DateTime($result_date_events[$x][1]);
-                $date_fin_SQL = new DateTime($result_date_events[$x][2]);
-    
-//On a aussi besoin de vérifier qu'une réservation ne se chevauche pas avec un créneau déjà pris. Je crée donc une variable i qui va de l'heure' de début à l'
-// heure de fin de l'event de l'utilisateur. Si sur l'intervalle, i matche avec un évènement de ma table, c'est que l'event de l'utilisateur va chevaucher un event 
-// déjà prévu. J'enlève 1 à l'heure'de fin car si par exemple on veut programmer un event de 15 à 16 et qu'il y en a déjà un de 16 à 17, ma boucle est erronnée.
-//On sépare les cas de création et de modification d'event car pour la modif, on a besoin d'exclure de la requête la réservation en question.
+            $date_debut_SQL = new DateTime($result_date_events[$x][1]);
+            $date_fin_SQL = new DateTime($result_date_events[$x][2]);
 
-                if(isset($_POST['reservation'])) {  
-                    if($_POST['end_time'] > $_POST['start_time']) {
+// Ici je vais avoir besoin de séparer les events sur une journée de ceux sur plusieurs jours. Pour l'event sur une journée je crée une boucle qui part de l'heure de départ 
+// à l'heure de fin et me renvoie l'erreur s'il y a un match dans la base de données. Pour l'event sur plusieurs journées je commence par une boucle avec la variable k qui va 
+//du jour de début au jour de fin.
+
+            if(isset($_POST['reservation'])) {    
+                
+                if($fin_event->format('Y-m-d') == $debut_event->format('Y-m-d')) {
+
+                    for($a = $debut_event->format('H'); $a < $fin_event->format('H'); $a++) {
+
+                        if($a == $date_debut_SQL->format('H') || $a == ($date_fin_SQL->format('H') - 1)) {
+                            $check = 0;
+                            $message = 'Votre réservation se chevauche sur une autre';
+                            break;
+                        }
+                    }
+                }
+                
+                elseif($fin_event->format('Y-m-d') > $debut_event->format('Y-m-d')) {
+
+                    for($k = $debut_event->format('d'); $k <= $fin_event->format('d'); $k++) {
+
+                        for($i = $debut_event->format('H'); $i < 20; $i++) {
+
+                            if($k == $date_debut_SQL->format('d') || $k == $date_fin_SQL->format('d')) {
+
+                                if($i == $date_debut_SQL->format('H') || $i == ($date_fin_SQL->format('H') - 1)) {
+                                    $check = 0;
+                                    $message = 'Votre réservation se chevauche sur une autre';
+                                    break;
+                                }
+                            }                       
+                        }
+
+                        for($j = ($fin_event->format('H') - 1 ); $j >= 8; $j--) {
+
+                            if($k == $date_debut_SQL->format('d') || $k == $date_fin_SQL->format('d')) {
+
+                                if($j == $date_debut_SQL->format('H') || $j == ($date_fin_SQL->format('H') - 1)) {
+                                    $check = 0;
+                                    $message = 'Votre réservation se chevauche sur une autre';
+                                    break;
+                                }
+                            }                       
+                        }
+                    }
+                }
+            }
     
-                        for($i = $_POST['start_time']; $i < $_POST['end_time']; $i++) {
-                            if($i == $date_debut_SQL->format('H') || $i == ($date_fin_SQL->format('H') - 1)) {
+            elseif(isset($_POST['modify'])) {  
+
+                if($fin_event->format('Y-m-d') == $debut_event->format('Y-m-d')) {
+
+                    for($a = $debut_event->format('H'); $a < $fin_event->format('H'); $a++) {
+    
+                        if($a == $date_debut_SQL->format('H') || $a == ($date_fin_SQL->format('H') - 1)) {
+
+// Pour les modifications d'event j'ai besoin de rajouter une condition qui exclue la résa en cours de ma requête sql.
+                            if($result_date_events[$x][0] !== $_SESSION['eventID']) {
                                 $check = 0;
                                 $message = 'Votre réservation se chevauche sur une autre';
                                 break;
                             }
                         }
-                    }  
-    
-                    if($date_debut_SQL->format('H') == $_POST['start_time'] || $date_fin_SQL->format('H') == $_POST['end_time']) {
-                        $check = 0;
-                        $message = 'Cette plage horaire est déjà réservée';
-                        break;
-                    }  
-    
+                    }
                 }
+                    
+                elseif($fin_event->format('Y-m-d') > $debut_event->format('Y-m-d')) {
+
+                    for($k = $debut_event->format('d'); $k <= $fin_event->format('d'); $k++) {
     
-                elseif(isset($_POST['modify'])) {  
-                    if($_POST['end_time'] > $_POST['start_time']) {
-    
-                        for($i = $_POST['start_time']; $i < $_POST['end_time']; $i++) {
-                            if($i == $date_debut_SQL->format('H') || $i == ($date_fin_SQL->format('H') - 1)) {
-                                if($result_date_events[$x][0] !== $_SESSION['eventID']) {
-                                    $check = 0;
-                                    $message = 'Votre réservation se chevauche sur une autre';
-                                    break;
+                        for($i = $debut_event->format('H'); $i < 20; $i++) {
+
+                            if($k == $date_debut_SQL->format('d') || $k == $date_fin_SQL->format('d')) {
+
+                                if($i == $date_debut_SQL->format('H') || $i == ($date_fin_SQL->format('H') - 1)) {
+
+                                    if($result_date_events[$x][0] !== $_SESSION['eventID']) {
+                                        $check = 0;
+                                        $message = 'Votre réservation se chevauche sur une autre';
+                                        break;
+                                    }
                                 }
-                            }
+                            }                       
                         }
-                    }  
-    
-                    if($date_debut_SQL->format('H') == $_POST['start_time'] || $date_fin_SQL->format('H') == $_POST['end_time']) {
-                        if($result_date_events[$x][0] !== $_SESSION['eventID']) {
-                            $check = 0;
-                            $message = 'Cette plage horaire est déjà réservée';
-                            break;
+        
+                        for($j = ($fin_event->format('H') - 1 ); $j >= 8; $j--) {
+                            
+                            if($k == $date_debut_SQL->format('d') || $k == $date_fin_SQL->format('d')) {
+
+                                if($j == $date_debut_SQL->format('H') || $j == ($date_fin_SQL->format('H') - 1)) {
+
+                                    if($result_date_events[$x][0] !== $_SESSION['eventID']) {
+                                        $check = 0;
+                                        $message = 'Votre réservation se chevauche sur une autre';
+                                        break;
+                                    }
+                                }
+                            }                       
                         }
-                    }  
-                }
-            }
-        }
+                    }
+                }    
+            }  
+        }           
+    }
     
  //Finalement si toutes les conditions sont réunies et $check est resté == 1, je crée/modifie mon event.      
         
@@ -244,7 +191,7 @@ function add_modify_event() {
                 $desc = mysqli_real_escape_string($mysqli, $_POST['desc']);
             
                 $request_add_event = "INSERT INTO reservations (titre, description, debut, fin, id_utilisateur) 
-                VALUES ('$titre', '$desc', '$date_debut', '$date_fin', '$_SESSION[userID]')";
+                VALUES ('$titre', '$desc', '$debut_event_formated', '$fin_event_formated', '$_SESSION[userID]')";
                 
                 $query_add_event = $mysqli->query($request_add_event);
     
@@ -256,7 +203,8 @@ function add_modify_event() {
                 $titre = mysqli_real_escape_string($mysqli, $_POST['titre']);
                 $desc = mysqli_real_escape_string($mysqli, $_POST['desc']);
             
-                $request_modify_event = "UPDATE reservations SET titre = '$titre', description =  '$desc', debut = '$date_debut' , fin = '$date_fin' WHERE id = '$_SESSION[eventID]'";
+                $request_modify_event = "UPDATE reservations 
+                SET titre = '$titre', description =  '$desc', debut = '$debut_event_formated' , fin = '$fin_event_formated' WHERE id = '$_SESSION[eventID]'";
                 
                 $query_modify_event = $mysqli->query($request_modify_event);
                 
